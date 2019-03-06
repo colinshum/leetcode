5a704223d708db04dc40452474a91750ea9958ff
Colin Shum <colin.a.shum@gmail.com>
2019-03-06 15:26:04 -0500

    add better solution for merging sorted arrays

--- ruby/easy/sort_and_search/merge_sorted_array.rb
+++ ruby/easy/sort_and_search/merge_sorted_array.rb
@@ -1,15 +1,12 @@
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/96/sorting-and-searching/587/
+
 # @param {Integer[]} nums1
 # @param {Integer} m
 # @param {Integer[]} nums2
 # @param {Integer} n
 # @return {Void} Do not return anything, modify nums1 in-place instead.
 def merge(nums1, m, nums2, n)
-    i = 0
-    
-    while (i < n) do
-        nums1.pop
-        i += 1
-    end
+    nums1.pop(n)
     
     nums2.each do |elem|
         nums1 << elem     
